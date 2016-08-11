2add649a10fdd7476c9a7bd0d4c347284b078a6a
Ruslan <hunter9999999@yandex.ru>
2016-08-11 15:22:34 +0400

    Add files via upload

--- job.py
+++ job.py
@@ -5,7 +5,7 @@ import os
 import errno
 import time
 
-working_dir = 'condor6'
+working_dir = 'condor_test'
 if os.path.exists(working_dir):
 	filenames = os.listdir(working_dir)
 	for filename in filenames:
@@ -13,36 +13,30 @@ if os.path.exists(working_dir):
 	os.rmdir(working_dir)
 time.sleep(1)
 os.makedirs(working_dir)
-os.rename('./analyt_mod', working_dir + '/analyt_mod')
+os.rename('./test', working_dir + '/test')
 time.sleep(1)
 os.chdir(working_dir)
 schedd = htcondor.Schedd()
-Tslot_max = 246140.0
 
-p = [0.0, 0.5]
-q = 1.0
-M = [32, 64, 100]
-Traw = [500.0 + i * 120.0 for i in range(2048)]
+args = [str(i) for i in range(100000)]
 
-for ip in p:
-	for iM in M:
-		for iTraw in Traw:
-			b = True
-			while b:
-				f = os.fork()
-				if f: # parent
-					r = os.wait()
-					if not r[1]:
-						b = False
-				else: # child
-					cmd = 'Glambda_Opt_AN_{}_{}_{}_{}'.format(str(iM), str(iTraw), str(ip), str(q))
-					print(cmd)
-					ad = classad.ClassAd({
-						'Cmd' : './analyt_mod',
-						'Arguments' : '{}'.format(cmd), 
-						'UserLog' : cmd + '.log', 
-						'Out' : cmd + '.out', 
-						'Err' : cmd + '.err', 
-						'ShouldTransferFiles' : 'YES'})
-					schedd.submit(ad)
-					os._exit(0)
+for iarg in args:
+	b = True
+	while b:
+		f = os.fork()
+		if f: # parent
+			r = os.wait()
+			if not r[1]:
+				b = False
+		else: # child
+			cmd = '{}'.format(iarg)
+			print(cmd)
+			ad = classad.ClassAd({
+				'Cmd' : './test',
+				'Arguments' : '{}'.format(cmd), 
+				'UserLog' : cmd + '.log', 
+				'Out' : cmd + '.out', 
+				'Err' : cmd + '.err', 
+				'ShouldTransferFiles' : 'YES'})
+			schedd.submit(ad)
+			os._exit(0)
