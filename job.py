d863c3edd12c453a815b79ab4f9ad523ea42769e
Ruslan <hunter9999999@yandex.ru>
2016-08-11 15:27:43 +0400

    документирование

--- job.py
+++ job.py
@@ -5,30 +5,30 @@ import os
 import errno
 import time
 
-working_dir = 'condor_test'
-if os.path.exists(working_dir):
+working_dir = 'condor_test' # рабочая директория
+if os.path.exists(working_dir): # если директория существует, удаляем её вместе со всеми файлами
 	filenames = os.listdir(working_dir)
 	for filename in filenames:
 		os.remove(working_dir + '/' + filename)
 	os.rmdir(working_dir)
 time.sleep(1)
-os.makedirs(working_dir)
-os.rename('./test', working_dir + '/test')
+os.makedirs(working_dir) # создаём из рабочую директорию
+os.rename('./test', working_dir + '/test') # переносим в директорию исполняемый файл
 time.sleep(1)
-os.chdir(working_dir)
+os.chdir(working_dir) # работаем из рабочей директории
 schedd = htcondor.Schedd()
 
-args = [str(i) for i in range(100000)]
+args = [str(i) for i in range(100000)] # аргументы программы
 
 for iarg in args:
-	b = True
+	b = True # входим в бесконечный цикл
 	while b:
 		f = os.fork()
-		if f: # parent
-			r = os.wait()
-			if not r[1]:
-				b = False
-		else: # child
+		if f: # родитель
+			r = os.wait() # ждём выполнения дочернего процесса
+			if not r[1]: # выполнение дочернего процесса завершилось без ошибок
+				b = False # выходим из бесконечного цикла
+		else: # дитя
 			cmd = '{}'.format(iarg)
 			print(cmd)
 			ad = classad.ClassAd({
