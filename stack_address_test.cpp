babcac5b6551631f5630793aa4e9ad8071e25fbf
Ruslan <hunter9999999@yandex.ru>
2016-08-15 14:42:47 +0400

    Update stack_address_test.cpp

--- stack_address_test.cpp
+++ stack_address_test.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
 
+#if TEST == 1
 void bar()
 {
-    int a;
-    std::cout << a << std::endl;
-    return;
+	int i;
+	std::cout << i << std::endl;
+	return;
 }
 
 void foo()
 {
-    int a = 3;
-    std::cout << a << std::endl;
-    return;
+	int i = 3;
+	std::cout << i << std::endl;
+	return;
 }
+#endif
+
+#if TEST == 2
+void bar()
+{
+	int i;
+	std::cout << &i << std::endl;
+	return;
+}
+
+void foo()
+{
+	int i = 3;
+	std::cout << &i << std::endl;
+	return;
+}
+#endif
+
+#if TEST == 3
+void bar()
+{
+	int i;
+	std::cout << i << " " << &i << std::endl;
+	return;
+}
+
+void foo()
+{
+	int i = 3;
+	std::cout << i << " " << &i << std::endl;
+	return;
+}
+#endif
+
 
 int main()
 {
-    foo();
-    bar();
-    return 0;
+	foo();
+	bar();
+	return 0;
 }
